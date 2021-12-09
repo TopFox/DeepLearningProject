e86aecec1d4c6b1c693ccbbd0413517a80a201a3
Arnaud <Arnaud.Savary@etu.unige.ch>
2021-12-09 16:45:54 +0100

    Start project 2

--- framework.py
+++ framework.py
@@ -1,6 +1,31 @@
 import torch
-from torch import Tensor, tanh
-import numpy as np
-import math
 
 torch.set_grad_enabled(False)
+
+
+class Module (object):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, * input):
+        raise NotImplementedError
+
+    def backward(self, * gradwrtoutput):
+        raise NotImplementedError
+
+    def param(self):
+        return []
+
+
+class Sequential(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, * input):
+        return
+
+    def backward(self):
+        return
+
+    def param(self):
+        return
