5ae8316c5aced43489b3d2a1842a8fb134ac983c
Jérémie Guy <guyjeremix@gmail.com>
2021-12-16 17:48:29 +0100

    Project 2 - Working version

--- Project2/framework.py
+++ Project2/framework.py
@@ -1,5 +1,6 @@
 from typing import ForwardRef
 import torch
+import math
 
 torch.set_grad_enabled(False)
 
@@ -15,95 +16,130 @@ class Module(object):
         raise NotImplementedError
 
     def param(self):
-        return []
+        return None
+
+    def zero_grad(self):
+        return None
 
 
-class Sequential(Module):  # TODO: do this bad boy
-    def __init__(self, * modules):
+class Sequential(Module):
+    def __init__(self, *modules):
         super().__init__()
+        self.name = 'Sequential'
         self.parameters = []
         self.modules = modules
+        self.sequence = []
 
-    def forward(self, *input):
-        return
+        for module in self.modules:
+            if module.name == 'Linear':
+                for parameter in module.param():
+                    self.parameters.append(parameter)
 
-    def backward(self):
-        return
+    # TODO understand this
+    def forward(self, input):
+        for index, module in enumerate(self.modules):
+            if module.name == 'Linear':
+                module.w = self.param()[index][0]
+                module.b = self.param()[index+1][0]
+            input = module.forward(input)
+            self.sequence.append(module.tensor)
+        return input
+
+    def backward(self, gradwrtoutput):
+        reversedModules = self.modules[::-1]
+        reversedSequence = self.sequence[::-1]
+        reversedParamaters = []
+
+        for index, module in enumerate(reversedModules):
+            module.tensor = reversedSequence[index]
+            gradwrtoutput = module.backward(gradwrtoutput)
+            if module.name == 'Linear':
+                for parameter in module.param()[::-1]:
+                    reversedParamaters.append(parameter)
+
+        self.parameters = reversedParamaters[::-1]
 
     def param(self):
         return self.parameters
 
+    def zero_grad(self):
+        for module in self.modules:
+            module.zero_grad()
+
 
 class Linear(Module):
-    def __init__(self, startDimension, endDimension, reLu=False):
+    def __init__(self, startDimension, endDimension, tanh=True):
         super().__init__()
+        self.name = 'Linear'
+
         self.startDimension = startDimension
         self.endDimension = endDimension
         self.tensor = torch.empty(1, 1)
-        # TODO : change those values
         # If we use tanh as activation function, it is better to initialize bias and weights using xavier
-        if reLu:
-            self.w = 0
-            self.b = 0
-            self.db = 0
-            self.dw = 0
+        if tanh:
+            std = math.sqrt(1./(startDimension+endDimension))
         # If we use ReLu as the activation function, we initialize with He
         else:
-            self.w = 0
-            self.b = 0
-            self.db = 0
-            self.dw = 0
+            std = math.sqrt(2./(startDimension+endDimension))
+        self.w = torch.empty(startDimension, endDimension).normal_(0, std)
+        self.b = torch.empty(endDimension).normal_(0, std)
+        self.dw = torch.zeros(startDimension, endDimension)
+        self.db = torch.zeros(endDimension)
+        self.tensor = torch.empty(1, 1)
 
-    def forward(self, *input):
+    def forward(self, input):
         # Chapter 3.6 slide 9 : forward is x*w + b
         self.tensor = input
         return input.mm(self.w) + self.b
 
-    def backward(self, *gradwrtoutput):
+    def backward(self, gradwrtoutput):
         """
         Scheme chapter 3.6 slide 9 :
         dl/dw = dl/ds * x^T
         dl/db = dl/ds
         """
         self.dw = self.tensor.t().mm(gradwrtoutput)
-        self.db = gradwrtoutput.mean(0)  # TODO D'où sort ce putain de mean ?!?
+        self.db = gradwrtoutput
 
         # Scheme chapter 3.6 slide 8 : backward is w^T * (dl/dx)
-        # return gradwrtoutput.mm(self.w.T())
-        return gradwrtoutput.matmul(self.w.T())
+        return gradwrtoutput.mm(self.w.t())
 
     def param(self):
-        return
+        return [[self.w, self.dw], [self.b, self.db]]
+
+    def zero_grad(self):
+        self.dw.zero_()
+        self.db.zero_()
 
 
 class ReLu(Module):
     def __init__(self):
         super().__init__()
         self.tensor = torch.empty(1, 1)
+        self.name = 'ReLu'
 
     def forward(self, input):
         # max(0,input)
         self.tensor = input
-        return torch.max(torch.zeros(input.size()), input)
+        return torch.max(input, torch.zeros(input.size()))
 
-    def backward(self, *gradwrtoutput):
+    def backward(self, gradwrtoutput):
         # Derivative of max(0,input) is 0 if input <= 0, else it is 1
-        if self.tensor <= 0:
-            return torch.tensor([[0.]])
-        return gradwrtoutput
+        return gradwrtoutput*(self.tensor > 0).float()
 
 
 class TanH(Module):
     def __init__(self):
         super().__init__()
         self.tensor = torch.empty(1, 1)
+        self.name = 'TanH'
 
     def forward(self, input):
         # tanh(input)
         self.tensor = input
         return input.tanh()
 
-    def backward(self, *gradwrtoutput):
+    def backward(self, gradwrtoutput):
         # Derivative of tanh(input) is 1/cosh^2(input)
         return gradwrtoutput * (1/torch.square(torch.cosh(self.tensor)))
 
@@ -111,9 +147,26 @@ class TanH(Module):
 class LossMSE(Module):
     def __init__(self):
         super().__init__()
+        self.name = 'LossMSE'
 
     def forward(self, input, prediction):
-        return torch.sum(torch.square(input - prediction)) # hot labels ??
+        prediction = torch.reshape(prediction, (len(input), 1))
+        return torch.sum(torch.square(input.sub(prediction)))
 
     def backward(self, input, prediction):
-        return 2 * (input - prediction)
+        prediction = torch.reshape(prediction, (len(input), 1))
+        return 2 * input.sub(prediction)
+
+
+class SGD():
+    def __init__(self, parameters, lr, wd):
+        self.name = 'SGD'
+        self.parameters = parameters
+        self.lr = lr
+        self.wd = wd
+
+    def step(self, parameters):
+        for parameter in parameters:
+            parameter[0] = parameter[0].sub(self.lr * parameter[1])
+            if self.wd:
+                parameter[0] -= 2*self.wd*parameter[0]
--- Project2/test.py
+++ Project2/test.py
@@ -1,3 +1,134 @@
 import framework
 from framework import *
-import torch
+import matplotlib.pyplot as plt
+
+
+def generateData(pointsNumber, plotPoints=True):
+    train_input = torch.rand(pointsNumber, 2)
+    train_target = (train_input.subtract(0.5).square().sum(
+        1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
+    test_input = torch.rand(pointsNumber, 2)
+    test_target = (test_input.subtract(0.5).square().sum(
+        1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
+
+    if plotPoints:
+        plt.title('Train data')
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
+                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        for i in range(pointsNumber):
+            if train_target[i] == 0:
+                plt.plot(train_input[i][0], train_input[i][1], 'ro')
+            else:
+                plt.plot(train_input[i][0], train_input[i][1], 'go')
+        plt.show()
+        plt.close()
+
+        plt.title('Test data')
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
+                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        for i in range(pointsNumber):
+            if test_target[i] == 0:
+                plt.plot(test_input[i][0], test_input[i][1], 'ro')
+            else:
+                plt.plot(test_input[i][0], test_input[i][1], 'go')
+        plt.show()
+    return train_input, train_target, test_input, test_target
+
+
+def train_model(model, criterion, train_input, train_target, test_input, test_target, mini_batch_size=50, nb_epochs=250, lr=5e-3, wd=1e-6, adaptative=0.9, plotPoints=True):
+    optimizer = framework.SGD(model.param(), lr=lr, wd=wd)
+    losses = []
+    for e in range(nb_epochs):
+
+        if adaptative:
+            epochs_drop = nb_epochs/5
+            lr = lr * math.pow(adaptative, math.floor((1+e)/epochs_drop))
+
+        acc_loss = 0
+        numberOfTrainErrors = 0
+        for b in range(0, train_input.size(0), mini_batch_size):
+            train_output = model.forward(train_input.narrow(0, b, mini_batch_size))
+            loss = criterion.forward(
+                train_output, train_target.narrow(0, b, mini_batch_size))
+            acc_loss += loss.item()
+
+            model.zero_grad()
+            model.backward(criterion.backward(
+                train_output, train_target.narrow(0, b, mini_batch_size)))
+            optimizer.step(model.param())
+
+            if e == nb_epochs-1:
+                for k in range(mini_batch_size):
+                    # if train_output[k] <= 0:
+                    #     train_output[k] = 0
+                    # else:
+                    #     train_output[k] = 1
+                    train_output[k] = round(float((train_output[k] + 1)/2))
+                    if train_target[b + k] != train_output[k]:
+                        numberOfTrainErrors += 1
+
+        acc_loss /= train_input.size(0)
+        losses.append(acc_loss)
+
+        test_prediction = []
+        numberOfTestErrors = 0
+        for b in range(0, test_input.size(0), mini_batch_size):
+            test_output = model.forward(test_input.narrow(0, b, mini_batch_size))
+
+            for k in range(mini_batch_size):
+                # if test_output[k] <= 0:
+                #     test_output[k] = 0
+                # else:
+                #     test_output[k] = 1
+                test_output[k] = round(float((test_output[k] + 1)/2))
+                if test_target[b + k] != test_output[k]:
+                    numberOfTestErrors += 1
+            if e == nb_epochs-1:
+                test_prediction.extend(test_output)
+        if e % 5 == 0:
+            print('Epoch:', e, ', number of errors in final test',
+                  numberOfTestErrors)
+    plt.title('Evolution of loss')
+    plt.plot(range(250), losses)
+    plt.show()
+
+    if plotPoints:
+        for i in range(test_input.size(0)):
+            if test_prediction[i] == 0:
+                plt.plot(test_input[i][0], test_input[i][1], 'ro')
+            else:
+                plt.plot(test_input[i][0], test_input[i][1], 'go')
+        plt.title('Predictions of the model')
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
+                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        plt.show()
+
+
+def test_framework():
+    # Parameters
+    nb_epochs = 250
+    mini_batch_size = 50
+
+    # Framework
+    linear1 = framework.Linear(2, 25, tanh=False)
+    linear2 = framework.Linear(25, 25)
+    linear3 = framework.Linear(25, 25)
+    linear4 = framework.Linear(25, 1)
+    relu = framework.ReLu()
+    tanh = framework.TanH()
+    criterion = framework.LossMSE()
+
+    # Data
+    train_input, train_target, test_input, test_target = generateData(
+        1000, plotPoints=False)
+
+    # Model
+    model = framework.Sequential(
+        linear1, relu, linear2, tanh, linear3, tanh, linear4, tanh)
+
+    # Train model
+    train_model(model, criterion, train_input, train_target, test_input,
+                test_target, mini_batch_size=mini_batch_size, nb_epochs=nb_epochs)
+
+
+test_framework()
