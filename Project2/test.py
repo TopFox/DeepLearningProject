b9808d19d7ca777718e66d26cab8c8951b8bfbf4
Jérémie Guy <guyjeremix@gmail.com>
2021-12-17 15:51:31 +0100

    Auto stash before merge of "main" and "origin/main"

--- Project2/test.py
+++ Project2/test.py
@@ -16,100 +16,137 @@ def generateData(pointsNumber, plotPoints=True):
     # Generating the training and testing points randomely and computing the class the belong to i.e. checking if their distance to the
     # center of the circle is smaller than 1/sqrt(2*pi)
     train_input = torch.rand(pointsNumber, 2)
-    train_target = (train_input.subtract(0.5).square().sum(
-        1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
+    train_target = (train_input.subtract(0.5).square().sum(1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
     test_input = torch.rand(pointsNumber, 2)
-    test_target = (test_input.subtract(0.5).square().sum(
-        1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
+    test_target = (test_input.subtract(0.5).square().sum(1).sqrt() <= (1/math.sqrt((2*math.pi)))).long()
 
     # Plotting the training data points with reference to the circle they should belong in
     if plotPoints:
         plt.title('Train data')
-        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
-                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /math.sqrt((2*math.pi)), color='k', alpha=0.2))
         for i in range(pointsNumber):
             if train_target[i] == 0:
+                # points inside the circle
                 plt.plot(train_input[i][0], train_input[i][1], 'ro')
             else:
+                # points outside the circle
                 plt.plot(train_input[i][0], train_input[i][1], 'go')
         plt.show()
-        plt.close()
+        
 
         # Plotting the training data points with reference to the circle they should belong in
         plt.title('Test data')
-        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
-                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /math.sqrt((2*math.pi)), color='k', alpha=0.2))
         for i in range(pointsNumber):
             if test_target[i] == 0:
+                # points inside the circle
                 plt.plot(test_input[i][0], test_input[i][1], 'ro')
             else:
+                # points outside the circle
                 plt.plot(test_input[i][0], test_input[i][1], 'go')
         plt.show()
 
     # the function returns the needed data
     return train_input, train_target, test_input, test_target
 
+# --------------------------------------- Training Model --------------------------------------
 
-def train_model(model, criterion, train_input, train_target, test_input, test_target, mini_batch_size=50, nb_epochs=250, lr=5e-3, wd=1e-6, mustPrint=True, plotLoss=True, plotPoints=True):
+# Main function to train the model to our problem
+def train_model(model, criterion, train_input, train_target, test_input, test_target, mini_batch_size=50, nb_epochs=250, lr=5e-3, wd=1e-6, plotPoints=True):
+    # initializing the optimizer
     optimizer = framework.SGD(model.param(), lr=lr, wd=wd)
     losses = []
+    
+    # Training for every epoch
     for e in range(nb_epochs):
-        acc_loss = 0
+        acc_loss = 0 # accumulated loss
         numberOfTrainErrors = 0
+
+        # We iteratively train using the batch size 
         for b in range(0, train_input.size(0), mini_batch_size):
-            train_output = model.forward(
-                train_input.narrow(0, b, mini_batch_size))
-            loss = criterion.forward(
-                train_output, train_target.narrow(0, b, mini_batch_size))
+
+            # runing the model and fetching its output
+            train_output = model.forward(train_input.narrow(0, b, mini_batch_size)) 
+            # computing the training loss
+            loss = criterion.forward(train_output, train_target.narrow(0, b, mini_batch_size))
             acc_loss += loss.item()
 
+            # Putting back the gradient to zero before computing the new one during backpropagation
             model.zero_grad()
-            model.backward(criterion.backward(
-                train_output, train_target.narrow(0, b, mini_batch_size)))
+
+            # computing said backpropagation
+            model.backward(criterion.backward(train_output, train_target.narrow(0, b, mini_batch_size)))
+
+            # optimizing the parameters with the learning rate (and, if activated, weight decay)
             optimizer.step(model.param())
 
+            # At the final epoch, we test the trained model for every batch on the train set
             if e == nb_epochs-1:
                 for k in range(mini_batch_size):
-                    train_output[k] = round(float((train_output[k] + 1)/2))
+                    # rounding the results to compute the output class prediction
+                    test_output[k] = round(float(test_output[k]))
+                    # if we have a wrong prediction, increment the error count
                     if train_target[b + k] != train_output[k]:
                         numberOfTrainErrors += 1
 
+        # Normalizing the accumulated loss and storing it
         acc_loss /= train_input.size(0)
         losses.append(acc_loss)
 
+        # At each epoch we want to see the evolution of the model performances on the test set
         test_prediction = []
         numberOfTestErrors = 0
+        
+        # We test the model for every batch on the test data  
         for b in range(0, test_input.size(0), mini_batch_size):
-            test_output = model.forward(
-                test_input.narrow(0, b, mini_batch_size))
+            # runing the model and fetching its output
+            test_output = model.forward(test_input.narrow(0, b, mini_batch_size))
 
             for k in range(mini_batch_size):
+                # rounding the results to compute the output class prediction
                 test_output[k] = round(float(test_output[k]))
+                # if we have a wrong prediction, increment the error count
                 if test_target[b + k] != test_output[k]:
                     numberOfTestErrors += 1
+            # At the last epoch we add the final test output to the model prediction
             if e == nb_epochs-1:
                 test_prediction.extend(test_output)
-        if e % 5 == 0 and mustPrint:
-            print('Epoch:', e, ', number of errors in final test',
-                  numberOfTestErrors)
-    if plotLoss:
-        plt.title('Evolution of loss')
-        plt.plot(range(250), losses)
-        plt.show()
+                
+        # Every 5 epoch print the test error
+        if e % 5 == 0:
+            print('Epoch:', e, ', percentage of errors in final test {:.2f}'.format(numberOfTestErrors/len(test_input)*100), '%')
+        
+    # Plotting the loss 
+    plt.title('Evolution of loss')
+    plt.ylabel('accumulated loss')
+    plt.xlabel('Epochs')
 
+    plt.plot(range(nb_epochs), losses)
+    plt.show()
+
+    # Plotting the visual representation of the output values
     if plotPoints:
         for i in range(test_input.size(0)):
             if test_prediction[i] == 0:
+                # points that are predicted as outside the circle
                 plt.plot(test_input[i][0], test_input[i][1], 'ro')
             else:
+                # points that are predicted as inside the circle
                 plt.plot(test_input[i][0], test_input[i][1], 'go')
-        plt.title('Predictions of the model')
-        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /
-                                       math.sqrt((2*math.pi)), color='k', alpha=0.2))
+        
+        title = 'Predictions of the model with :\n' + ' lr=' + str(lr) + ', wd=' + str(wd) + ' and mini_batch_size=' + str(mini_batch_size)
+        plt.title(title)
+        plt.xlabel('x coordinates')
+        plt.ylabel('y coordinates')
+        
+        # reference circle
+        plt.gca().add_patch(plt.Circle((0.5, 0.5), 1 /math.sqrt((2*math.pi)), color='k', alpha=0.2))
         plt.show()
 
+# --------------------------------------- Executing training and testing --------------------------------------
     return numberOfTestErrors
 
+# we define here all of the payers, parameters and settings to reun the model
 
 def model_tuning(number_of_runs=5):
     # Fixed parameters
@@ -177,17 +214,18 @@ def test_framework():
     nb_epochs = 250
     mini_batch_size = 50
 
-    # Framework
+    # Framework - hidden layers
     linear1 = framework.Linear(2, 25, tanh=False)
     linear2 = framework.Linear(25, 25)
     linear3 = framework.Linear(25, 25)
     linear4 = framework.Linear(25, 1)
     criterion = framework.LossMSE()
 
-    # Data
-    train_input, train_target, test_input, test_target = generateData(
-        1000, plotPoints=False)
+    # Generating the data
+    train_input, train_target, test_input, test_target = generateData(1000, plotPoints=True)
 
+    # Creating the model
+    model = framework.Sequential(linear1, relu, linear2, tanh, linear3, tanh, linear4, relu)
     # Model tuning
     best_lr, best_wd, best_act_fun_1, best_act_fun_2, best_act_fun_3, best_act_fun_4 = model_tuning()
 
@@ -195,6 +233,8 @@ def test_framework():
     model = framework.Sequential(
         linear1, best_act_fun_1, linear2, best_act_fun_2, linear3, best_act_fun_3, linear4, best_act_fun_4)
 
+    # Training the model
+    train_model(model, criterion, train_input, train_target, test_input, test_target, mini_batch_size=mini_batch_size, nb_epochs=nb_epochs)
     # Train model
     _ = train_model(model, criterion, train_input, train_target, test_input,
                     test_target, mini_batch_size=mini_batch_size, nb_epochs=nb_epochs, lr=best_lr, wd=best_wd)
